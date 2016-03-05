d6be7cb9989b26ecc37bfd36a9c813a33a8b6725
David Boisits <david.boisits@technikum-wien.at>
2016-03-05 17:14:05 +0100

    add projct Task4

---
+++ tasks/Task4/Task4/Task2/Task2/IMitarbeiter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    interface IMitarbeiter
+    {
+        string firstname { get; set; }
+        string lastname { get; set; }
+        int svn { get; }
+        double salary { get; set; }
+        double popularity { get; set; }
+
+
+        string changefirstname();
+        string changelastname();
+        double promote(double salary);
+        double popularity_rate(double value);
+    }
+}
